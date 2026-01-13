f42bd2eec57d118904ac824372e17e4969b87d0c
valentinazag <valentinazagman@gmail.com>
2026-01-13 13:00:21 +0000

    organizations and projects endpoints

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,17 +1,4 @@
-import { Welcome } from "../welcome/welcome";
+import * as v from "valibot";
 import type { Route } from "./+types/home";
-
-export function meta() {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
-
-export function loader({ context }: Route.LoaderArgs) {
-  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
-}
-
-export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
-}
+import { OrganizationSchema } from "./organizations";
+import { type Project, ProjectSchema } from "./projects";
